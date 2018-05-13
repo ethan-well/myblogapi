0456d246c13fde9474031ee93a47a5ced7a09c72
魏巍 <wewin11235@163.com>
2018-05-13 15:36:00 +0800

    add seeds of category

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+['JavaScript', 'Ruby', 'React', 'Linux', 'DB'].each do |category|
+  Category.create(name: category)
+end
