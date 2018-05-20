8ef86ca9239e77d42f3c056838d6919f3ab4c295
魏巍 <wewin11235@163.com>
2018-05-20 15:09:30 +0800

    style(api): make the api routes more RESTful

--- app/api/blog_site/api.rb
+++ app/api/blog_site/api.rb
@@ -15,14 +15,14 @@ module BlogSite
     end
 
     resource :articles do
-      # example /api/articles/create
+      # example /api/articles
       desc 'create blog'
       params do
         requires :title, type: String, desc: 'Article title, type: String'
         requires :content, type: String, desc: 'Article content, type: Text'
         requires :category, type: Integer, desc: 'Article category Id, type: Integer'
       end
-      post :create do
+      post do
         article = Article.new(title: params[:title], content: params[:content], category_id: params[:category])
         if article.save
           { status: 1, msg: 'create article success', id: article.id }
@@ -31,12 +31,12 @@ module BlogSite
         end
       end
 
-      # example /api/articles/show
+      # example /api/articles/:id
       desc 'show blog'
       params do
         requires :id, type: Integer, desc: 'Article id, type: Integer'
       end
-      get :show do
+      get ':id' do
         article = Article.find(params[:id])
         if article.present?
           {status: 1, msg: 'get article success'}.merge({article: article.attributes})
@@ -45,9 +45,9 @@ module BlogSite
         end
       end
 
-      # example /api/articles/get_lists
+      # example /api/articles
       desc 'get create list'
-      get :get_lists do
+      get do
         Article.order(created_at: :desc).limit(10).map do |article|
           article.attributes.merge({comment_count: article.comments.count})
         end
@@ -63,7 +63,6 @@ module BlogSite
       end
       put ':id' do
         begin
-          puts "category_id: #{params[:category_id]}"
           article = Article.find(params[:id])
           article.update_attributes!(title: params[:title], content: params[:content], category_id: params[:category])
           { status: 1, msg: 'update article success', id: article.id }
@@ -75,21 +74,33 @@ module BlogSite
 
 
     resource :categories do
-      # example /api/categories/get_lists
+      # example /api/categories
       desc 'get category list'
-      get :get_lists do
+      get do
         {status: 1, categories: Category.all}
+      end
+
+      resource ':id' do
+        # example /api/categories/:id/articles
+        get 'articles' do
+          category = Category.find(params[:id])
+          if category.present?
+            {status: 1, msg: 'get article list success', articles: category.articles.map(&:attributes)}
+          else
+            {status: 0, mes: 'get article list failed'}
+          end
+        end
       end
     end
 
     resource :category_articles do
-      # example /api/category_articles/get_lists
+      # example /api/category_articles/:id
       desc "get articles list of one category"
       params do
-        requires :category, type: Integer, desc: 'Article category, type: Integer'
+        requires :id, type: Integer, desc: 'Category ID, type: Integer'
       end
-      get :get_lists do
-        category = Category.find(params[:category])
+      get ':id' do
+        category = Category.find(params[:id])
         if category.present?
           {status: 1, msg: 'get article list success', articles: category.articles.map(&:attributes)}
         else
