c1aa3e6aff00547db06c5a290569a59af1a9a08e
魏巍 <wewin11235@163.com>
2018-07-21 08:54:34 +0800

    feat(cors): 简单请求

--- app/controllers/myblog_controller.rb
+++ app/controllers/myblog_controller.rb
@@ -1,9 +1,12 @@
 class MyblogController < ApplicationController
   protect_from_forgery except: :test
+
   def index
   end
   
   def test
+    headers['Access-Control-Allow-Origin'] = "*"
+    headers['Access-Control-Request-Method'] = %w{GET POST OPTIONS}.join(",")
     respond_to do |format|
       format.js do
         render json: {test2: 'test2'}, :callback => params[:callback]
